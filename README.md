01b5e0094dba37e3cb98222a6f22753d47062cf3
Bokang Makibinye <98650142+devorbs@users.noreply.github.com>
2023-12-17 22:18:47 -0800

    First upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# Tile Matcher
+
+Tile matcher is a simple web-based memory game implemented in HTML, CSS, and JavaScript.
+
+## Table of Contents
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [How to Play](#how-to-play)
+- [Development](#development)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+- Grid of clickable/touchable boxes
+- Matching pairs of boxes disappear
+- Responsive design for various screen sizes
+
+## Getting Started
+
+1. **Clone the repository:**
+   ```bash
+   git clone https://github.com/devorbs/tile-matcher.git
+   cd tile-matcher
+
+# Run the application:
+Simply open 'index.html' in browser or go to: 
+
+# How to Play
+Click or touch the boxes to reveal their colors.
+Match pairs of boxes with the same color to make them disappear.
+The game ends when all pairs are matched.
+
+# Development
+Tech Stack
+HTML
+CSS
+JavaScript (ES6+)
+
+# Contributing
+Fork the repository.
+Create a new branch: git checkout -b feature/your-feature-name.
+Commit your changes: git commit -m 'Add some feature'.
+Push to the branch: git push origin feature/your-feature-name.
+Submit a pull request.
+
+# License
+This project is licensed under the MIT License.
+
---
+++ app.js
@@ -0,0 +1,87 @@
+class MemoryGame {
+    constructor(size, colors) {
+        this.size = size;
+        this.colors = [...colors, ...colors].sort(() => Math.random() - 0.5);
+        this.selectedBoxes = [];
+        this.createGrid();
+    }
+
+    createGrid() {
+        const app = document.getElementById('app');
+        const tiles = new Array(this.size).fill(null).map(() => new Array(this.size).fill(null));
+
+        for (let row = 0; row < tiles.length; row++) {
+            for (let col = 0; col < tiles[row].length; col++) {
+                const color = this.colors.pop();
+                const box = new Box(row, col, color);
+
+                let boxElement = document.createElement('div');
+                boxElement.id = `box-${row}-${col}`;
+                boxElement.classList = "box";
+                app.appendChild(boxElement);
+
+                boxElement.addEventListener('click', () => {
+                    this.handleBoxClick(boxElement, box);
+                });
+
+                boxElement.addEventListener('touchstart', (event) => {
+                    event.preventDefault(); 
+                    handleBoxClick(boxElement, box);
+                });
+            }
+        }
+    }
+
+    handleBoxClick(boxElement, box) {
+        if (this.selectedBoxes.length < 2 && !this.isBoxSelected(box)) {
+            this.selectedBoxes.push({ element: boxElement, box: box });
+            boxElement.style.backgroundColor = box.getColor();
+
+            if (this.selectedBoxes.length === 2) {
+                this.checkSelectedBoxes();
+            }
+        }
+    }
+
+    isBoxSelected(box) {
+        return this.selectedBoxes.some(selected => selected.box === box);
+    }
+
+    checkSelectedBoxes() {
+        if (this.selectedBoxes[0].box.getColor() === this.selectedBoxes[1].box.getColor()) {
+            setTimeout(() => {
+                this.selectedBoxes.forEach(selected => {
+                    selected.element.style.opacity = 0;
+                });
+                this.selectedBoxes = [];
+            }, 800);
+        } else {
+            setTimeout(() => {
+                this.selectedBoxes.forEach(selected => {
+                    selected.element.style.backgroundColor = "black";
+                });
+                this.selectedBoxes = [];
+            }, 800);
+        }
+    }
+}
+
+class Box {
+    constructor(row, col, color) {
+        this.row = row;
+        this.col = col;
+        this.color = color;
+    }
+
+    getColor() {
+        return this.color;
+    }
+
+    getPos() {
+        return [this.row, this.col];
+    }
+}
+
+const size = 4;
+const colors = ['darkgreen', 'darkmagenta', 'darkred', 'dodgerblue', 'lawngreen', 'orchid', 'sienna', 'saddlebrown'];
+const game = new MemoryGame(size, colors);
